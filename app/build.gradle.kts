223e8dff24c3d0810c9aa2a15b567d0c9a784aab
Yuval Itzhak <yuval.itzhak3@gmail.com>
2025-01-07 22:38:55 +0200

    changing the toolbar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,8 @@ dependencies {
     implementation("androidx.navigation:navigation-ui:$nav_version")
     implementation ("androidx.room:room-runtime:2.6.1")
     implementation ("androidx.room:room-ktx:2.6.1")
+    implementation ("com.google.android.material:material:1.12.0")
+    implementation ("androidx.appcompat:appcompat:1.6.1")
     kapt ("androidx.room:room-compiler:2.6.1")
     implementation(libs.glide)
     testImplementation(libs.junit)
--- app/src/main/java/com/example/architectureproject/ui/MainActivity.kt
+++ app/src/main/java/com/example/architectureproject/ui/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.architectureproject.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.architectureproject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Find the Toolbar
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
     }
 }
